b4a50de5d20d14cd55ca21526dde55c40945d2a8
Elena Czubiak <elenaczubiak@gmail.com>
2017-09-19 00:16:38 -0700

    Update two recursion functions to pass tests

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+ var bodyElements = document.body;
+  var classElements = [];
+
+  var getElements = function(node) {
+    var nodeClassName = node.className;
+    if (nodeClassName !== undefined && nodeClassName.indexOf(className) > -1) {
+      classElements.push(node);
+    }
+
+    if (!node.hasChildNodes) {
+        return;
+    } else {
+      node.childNodes.forEach(function(item) {
+        return getElements(item);
+      });
+    }
+  };
+  
+  getElements(bodyElements);
+  
+  return classElements;
+
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+    var type = typeof obj;
+    
+      if (['number', 'boolean', 'symbol'].indexOf(type) > -1) {
+        return obj.toString();
+      } 
+
+        else if (type === 'string') {
+          return '"' + obj + '"';
+      }
+
+        else if (obj === '') {
+        return '';
+      }
+
+        else if (['function', 'undefined'].indexOf(type) > -1) {
+        return '';
+      } 
+
+        else if (obj === null) {
+        return 'null';
+      } 
+
+        else if (Array.isArray(obj)) {
+          var arrString = obj.map(function(element) {
+            return stringifyJSON(element);
+          }).join(',');
+          return  '[' + arrString + ']';
+        } 
+          else {
+          var objStrings = [];
+          for (var key in obj) {
+            if (typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+              objStrings.push('"' + key + '"' + ':' + stringifyJSON(obj[key]));
+            }
+          }
+          return '{' + objStrings.join(',') + '}';
+      }
+
+    };
+
+
+console.log(stringifyJSON([1, 2, {'a': 3}, 'b']));
